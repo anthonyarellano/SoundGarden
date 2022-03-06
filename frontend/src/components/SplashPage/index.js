1d73a1013165706fcfe6d1c13e499abb97bb0d22
anthony <theanthonyarellano@gmail.com>
2022-03-05 20:07:18 -0500

    added animated circle to splash page + background color + fixed scroll issues

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -3,6 +3,10 @@ import './style/splashpage.css';
 const SplashPage = () => {
     return (
         <div className='splashPage'>
+            <img
+                src={require("./style/splashRing.png")}
+                className="pic">
+            </img>
         </div>
     )
 };
