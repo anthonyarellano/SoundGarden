cd4f20e71b4df2adc0695196251b0174b57978fe
anthony <theanthonyarellano@gmail.com>
2022-03-07 12:53:37 -0500

    model associations done

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -94,7 +94,9 @@ module.exports = (sequelize, DataTypes) => {
    };
 
   User.associate = function(models) {
-    // associations can be defined here
+    User.hasMany(models.Song, { foreignKey: 'userId' });
+    User.hasMany(models.Playlist, { foreignKey: 'userId' });
+    User.hasMany(models.Comment, { foreignKey: 'userId' });
   };
 
 
