04083b67edde4f870f94cc666f9ff1e4a8e2ce48
anthony <theanthonyarellano@gmail.com>
2022-03-07 12:44:29 -0500

    added songsRouter to api index.js

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,11 +4,11 @@ const asyncHandler = require('express-async-handler');
 const { User } = require('../../db/models');
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
-
+const songsRouter = require('./songs.js')
 
 router.use('/session', sessionRouter);
-
 router.use('/users', usersRouter);
+router.use('/songs', songsRouter)
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
--- backend/routes/api/songs.js
+++ backend/routes/api/songs.js
@@ -1,3 +1,18 @@
 const express = require('express')
 const asyncHandler = require('express-async-handler');
 const { User } = require('../../db/models');
+
+const router = express.Router();
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
