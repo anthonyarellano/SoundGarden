5b3a6e62f2711c7ca08c7d0fdabfa30405fb7a53
anthony <theanthonyarellano@gmail.com>
2022-03-08 13:50:43 -0500

    finished songbuttons component

--- frontend/src/components/UserProfile/SongButtons.js
+++ frontend/src/components/UserProfile/SongButtons.js
@@ -1,18 +1,18 @@
 const SongButtons = () => {
 
     return (
-        <div>
+        <div className='song-button-container'>
             <div className="song-play-button">
-                <img></img>
+                <img className="song-image" src={require('./style/images/play-button.png')}></img>
             </div>
             <div className="song-edit-button">
-                <img></img>
+                <img className="song-image edit" src={require('./style/images/edit-button.png')}></img>
             </div>
             <div className="song-delete-button">
-                <img></img>
+                <img className="song-image delete" src={require('./style/images/delete-button.png')}></img>
             </div>
             <div className="create-playlist-button">
-                <img></img>
+                <img className="song-image playlist" src={require('./style/images/playlist-button.png')}></img>
             </div>
         </div>
     )
