4c3f4bf4376bc42f73690a06571110e9d07a4836
anthony <theanthonyarellano@gmail.com>
2022-03-08 19:49:36 -0500

    minor bug fix

--- frontend/src/components/UserProfile/PlaylistContainer.js
+++ frontend/src/components/UserProfile/PlaylistContainer.js
@@ -7,9 +7,6 @@ const PlaylistContainer = ({sessionUser}) => {
     const dispatch = useDispatch();
     const playlists = useSelector((state) => Object.values(state.playlists));
 
-    console.log("HERE1", playlists);
-    const songList = playlists[0].Songs;
-    console.log(playlists);
     useEffect(() => {
         dispatch(getPlaylists(sessionUser.id))
     }, [dispatch])
