86ff35849f6995fce85a186889f3641cfbfcc7a4
anthony <theanthonyarellano@gmail.com>
2022-03-09 09:13:11 -0500

    added minor styling to playlist cards

--- frontend/src/components/UserProfile/PlaylistCard.js
+++ frontend/src/components/UserProfile/PlaylistCard.js
@@ -5,12 +5,10 @@ const PlaylistCard = ({songs}) => {
     return (
         <div className="playlist-card-container">
             {songs?.map((song) => (
-            <div className='playlist-card-wrapper'>
                 <div className='playlist-card'>
                     <img src={song?.imgUrl} style={{height: "25px"}}></img>
                     <p className='playlist-card-song-title'>{song?.title}</p>
                 </div>
-            </div>
             ))}
         </div>
     )
