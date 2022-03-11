701cdb5ed90e8fd95b2bc38fa53193d55cf8e9d5
anthony <theanthonyarellano@gmail.com>
2022-03-11 16:52:39 -0500

    artists loaded into discover page

--- frontend/src/components/DiscoverPage/ArtistCard.js
+++ frontend/src/components/DiscoverPage/ArtistCard.js
@@ -1,15 +1,38 @@
 import { getArtists } from "../../store/artists";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from 'react';
+import './style/discoverpage.css';
 
 const ArtistCard = () => {
+    const artists = useSelector((state) => Object.values(state.artists));
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getArtists());
+        console.log(artists);
+    }, [dispatch])
+
 
     return (
         <>
-            <h1>Artist Card</h1>
-            <div className="artist-card-container">
-                <p
-                    onClick={() => dispatch(getArtists())}>click</p>
+            <p
+                className="discover-page-header"
+                >Browse our Artists</p>
+            <div
+                className="artist-card-container">
+               {!artists &&
+               <h1
+                className="discover-page-header">Loading Artists...</h1>}
+               {artists?.map((artist) => (
+                   <div
+                    className="artist-card"
+                   >
+                       <img
+                            className="artist-card-image"
+                            src={artist?.imgUrl}
+                       />
+                   </div>
+               ))}
             </div>
         </>
     )
