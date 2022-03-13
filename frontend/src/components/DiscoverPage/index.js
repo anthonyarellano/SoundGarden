72f8ae4967b1af6bedf35e107cf4b6139df3d04c
anthony <theanthonyarellano@gmail.com>
2022-03-13 10:13:49 -0400

    added text input for search

--- frontend/src/components/DiscoverPage/index.js
+++ frontend/src/components/DiscoverPage/index.js
@@ -1,16 +1,31 @@
 import { Redirect } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ArtistCard from './ArtistCard';
+import { useState } from 'react';
 
 const DiscoverPage = ({setAllActive, setStyle}) => {
     const sessionUser = useSelector(state => state.session.user);
+    const [term, setTerm] = useState("");
 
     if (!sessionUser) {
         return <Redirect to="/" />
     }
 
+    const handleSearch = (e) => {
+
+
+    };
+
+
     return (
         <>
+            <input
+                type="text"
+                placeholder="Search SoundGarden"
+                value={term}
+                onChange={(e) => {
+                    setTerm(e.target.value)
+                    handleSearch()}}/>
             <ArtistCard setAllActive={setAllActive} setStyle={setStyle}/>
         </>
     )
