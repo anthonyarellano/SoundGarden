a7beb807e6a725e17a7cd03b83ff67e68c01b00c
anthony <theanthonyarellano@gmail.com>
2022-03-27 21:43:24 -0400

    began error handling for comment submission

--- frontend/src/components/UserProfile/CommentContainer.js
+++ frontend/src/components/UserProfile/CommentContainer.js
@@ -1,21 +1,46 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import CommentCard from './CommentCard';
 import './style/commentContainer.css';
 
 const CommentContainer = () => {
     const [comment, setComment] = useState("");
+    const [hasSubmitted, setHasSubmitted] = useState(false);
+    const [errors, setErrors] = useState([]);
+    
+    useEffect(() => {
+        let errors = [];
+        if (hasSubmitted) {
+            if (comment.length <= 0) errors.push('Please enter a value for comment.')
+        }
+        setErrors(errors);
+    }, [comment])
+
+    const handleSubmit = () => {
+        setHasSubmitted(true);
+
+    };
 
     return (
         <div className='comment-container'>
             <div
                 className='comment-input-container'
-                >
+            >
                 <textarea
                     rows={5}
                     cols={50}
-                    style={{resize: "None"}}
+                    style={{resize: "None",
+                            fontFamily: "Interstate Light Cond",
+                            margin: "0px",
+                            padding: "0px"}}
                     onChange={(e) => setComment(e.target.value)}
                 />
+                <div
+                    style={{border: "1px solid #F5F5F5",
+                            margin: "0px",
+                            cursor: "pointer"}}
+                    onClick={handleSubmit}>
+                    Submit
+                </div>
             </div>
             <h1>Comment Container!</h1>
             <CommentCard />
