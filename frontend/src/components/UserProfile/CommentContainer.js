45d534182c6d7e1cdee09ae0aaf0b286639a83b5
anthony <theanthonyarellano@gmail.com>
2022-03-27 21:19:13 -0400

    added comment textarea that is not resizable by user

--- frontend/src/components/UserProfile/CommentContainer.js
+++ frontend/src/components/UserProfile/CommentContainer.js
@@ -1,9 +1,20 @@
+import CommentCard from './CommentCard';
 import './style/commentContainer.css';
 
 const CommentContainer = () => {
     return (
         <div className='comment-container'>
+            <div
+                className='comment-input-container'
+                >
+                <textarea
+                    rows={5}
+                    cols={50}
+                    style={{resize: "None"}}
+                />
+            </div>
             <h1>Comment Container!</h1>
+            <CommentCard />
         </div>
     )
 };
